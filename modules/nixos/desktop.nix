da181ebfa041838347846fb71e7b8422cd8ddb94
liqihao <liqiha0@outlook.com>
2025-08-16 17:02:14 +0800

    chore: initial commit

---
+++ modules/nixos/desktop.nix
@@ -0,0 +1,60 @@
+{ pkgs, ... }:
+
+{
+  # services.displayManager.sddm = {
+  #   enable = true;
+  #   wayland.enable = true;
+  # };
+
+  # services.displayManager = {
+  #   defaultSession = "niri";
+  #   autoLogin = {
+  #     enable = true;
+  #     user = "liqihao";
+  #   };
+  # };
+
+  programs = {
+    hyprland = {
+      enable = true;
+      package = pkgs.unstable.hyprland;
+    };
+    niri = {
+      enable = true;
+      package = pkgs.unstable.niri;
+    };
+  };
+
+  hardware.graphics.enable = true;
+
+  fonts.packages = with pkgs; [
+    noto-fonts
+    noto-fonts-cjk-sans
+  ];
+
+  virtualisation.waydroid.enable = true;
+
+  programs = {
+    dank-material-shell.greeter = {
+      enable = true;
+      compositor.name = "niri";
+      quickshell.package = pkgs.unstable.quickshell;
+    };
+    _1password-gui = {
+      enable = true;
+      package = pkgs.unstable._1password-gui;
+      polkitPolicyOwners = [ "liqihao" ];
+    };
+    _1password.enable = true;
+  };
+
+  environment.etc = {
+    "1password/custom_allowed_browsers" = {
+      text = ''
+        zen
+        .zen-wrapped
+      '';
+      mode = "0755";
+    };
+  };
+}
